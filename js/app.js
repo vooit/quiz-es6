ee0832718a12157ac4ad28b3826ecd12928d7e4f
Wojtek <wojciechpac@gmail.com>
2017-09-20 11:24:52 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -128,21 +128,23 @@
     function clear() {
         clearInterval(timer);
     }
+    /* --------TIMER END--------- */
 
-
-
+    /* --------GAME--------- */
     function buildQuiz() {
         // we'll need a place to store the HTML output
         const output = [];
 
-        // for each question...
+
         myQuestions.forEach((currentQuestion, questionNumber) => {
-            // we'll want to store the list of answer choices
+            // for each question...
+            // set store for answers
             const answers = [];
 
-        // and for each available answer...
+
         for (letter in currentQuestion.answers) {
-            // ...add an HTML radio button
+            // and for each available answer...
+            // add an HTML radio button
             answers.push(
                 `<label>
 			  <!--(${letter})-->
@@ -250,105 +252,3 @@
     previousButton.addEventListener("click", showPreviousSlide);
     nextButton.addEventListener("click", showNextSlide);
 })();
-
-
-/* ----------- TIMER ----------- */
-//
-// function getTimeRemaining(endtime) {
-//     var t = Date.parse(endtime) - Date.parse(new Date());
-//     var seconds = Math.floor((t / 1000) % 60);
-//     var minutes = Math.floor((t / 1000 / 60) % 60);
-//     var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-//     var days = Math.floor(t / (1000 * 60 * 60 * 24));
-//     return {
-//         'total': t,
-//         'days': days,
-//         'hours': hours,
-//         'minutes': minutes,
-//         'seconds': seconds
-//     };
-// }
-//
-// function initializeClock(id, endtime) {
-//     var clock = document.getElementById(id);
-//     var daysSpan = clock.querySelector('.days');
-//     var hoursSpan = clock.querySelector('.hours');
-//     var minutesSpan = clock.querySelector('.minutes');
-//     var secondsSpan = clock.querySelector('.seconds');
-//
-//     function updateClock() {
-//         var t = getTimeRemaining(endtime);
-//
-//         daysSpan.innerHTML = t.days;
-//         hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-//         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-//         secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-//
-//         if (t.total <= 0) {
-//             clearInterval(timeinterval);
-//         }
-//     }
-//
-//     updateClock();
-//     var timeinterval = setInterval(updateClock, 1000);
-// }
-//
-// var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-// // if there's a cookie with the name myClock, use that value as the deadline
-// if(document.cookie && document.cookie.match('myClock')){
-//     // get deadline value from cookie
-//     var deadline = document.cookie.match(/(^|;)myClock=([^;]+)/)[2];
-// }
-//
-// // otherwise, set a deadline 10 minutes from now and
-// // save it in a cookie with that name
-// else{
-//     // create deadline 10 minutes from now
-//     var timeInMinutes = 60;
-//     var currentTime = Date.parse(new Date());
-//     var deadline = new Date(currentTime + timeInMinutes*60*1000);
-//
-//     // store deadline in cookie for future reference
-//     document.cookie = 'myClock=' + deadline + '; path=/; domain=tcafe.net';
-// }
-// initializeClock('clockdiv', deadline);
-// var timeInMinute = setTimeout(myFunction, 3600000);
-// function myFunction() {
-//     alert('Time up');
-// }
-//
-//
-// function setCookie(cname,cvalue,exdays) {
-//     var d = new Date();
-//     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-//     var expires = "expires=" + d.toGMTString();
-//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-// }
-//
-// function getCookie(cname) {
-//     var name = cname + "=";
-//     var decodedCookie = decodeURIComponent(document.cookie);
-//     var ca = decodedCookie.split(';');
-//     for(var i = 0; i < ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) == ' ') {
-//             c = c.substring(1);
-//         }
-//         if (c.indexOf(name) == 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return "";
-// }
-// function checkCookie() {
-//     var user=getCookie("username");
-//     if (user != "") {
-//         alert("Welcome again " + user);
-//         document.write("Hello, " + user + "!");
-//     } else {
-//         user = prompt("Please enter your name:","");
-//         if (user != "" && user != null) {
-//             setCookie("username", user, 30);
-//         }
-//     }
-// }
